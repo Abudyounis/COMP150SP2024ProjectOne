e0251b3eed9b3b37a6c66a7cef8bb918d1d53c64
Abudyounis <132238281+Abudyounis@users.noreply.github.com>
2024-04-16 23:06:43 -0500

    Update app.py

--- project_code/test/app.py
+++ project_code/test/app.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template, request, session
+import game
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/start_game', methods=['POST'])
+def start_game():
+    session['game'] = game.init_game()
+    return {'message': 'Game started!'}
+
+@app.route('/perform_action', methods=['POST'])
+def perform_action():
+    action = request.form.get('action')
+    output = game.perform_action(session['game'], action)
+    return {'output': output}
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
